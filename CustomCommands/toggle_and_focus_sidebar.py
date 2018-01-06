581a91d441800dbf855a487c12ecb0822c74f781
Jake Park <danbi2990@gmail.com>
2018-01-06 22:02:18 +0900

    Change for mac

--- CustomCommands/toggle_and_focus_sidebar.py
+++ CustomCommands/toggle_and_focus_sidebar.py
@@ -20,5 +20,16 @@ class ToggleAndFocusSidebar(sublime_plugin.WindowCommand):
             self.window.run_command("toggle_side_bar")
         else:
             self.previous_group = self.window.active_group()
-            is_revealed(self.window) or self.window.run_command("toggle_side_bar")
+            is_revealed(self.window) \
+                or self.window.run_command("toggle_side_bar")
             self.window.run_command("focus_side_bar")
+
+
+class RevealAndFocusSidebar(sublime_plugin.WindowCommand):
+
+    def __init__(self, window):
+        super().__init__(window)
+
+    def run(self):
+        self.window.run_command("reveal_in_side_bar")
+        self.window.run_command("focus_side_bar")
